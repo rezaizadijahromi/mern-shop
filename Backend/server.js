ba0bd3db4eabff4ea886e3074d536a39b1cb94ab
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-04-01 13:31:28 +0430

    Frontend not started yet but backend complete models and now working on controllers and routes

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+import colors from "colors";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold,
+  ),
+);
