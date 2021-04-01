dafd98163ce2fabbed4172ae6074b9fc0a06c1d5
rezaizadijahromi <rezaizadij2000@gmail.com>
2021-04-01 21:19:31 +0430

    update

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,19 @@
 import express from "express";
-import { authUser, registerUser } from "../controllers/userControllers.js";
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userControllers.js";
+import { admin, protect } from "../middlewares/authMiddleWare.js";
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.post("/login", authUser);
 
 export default router;
