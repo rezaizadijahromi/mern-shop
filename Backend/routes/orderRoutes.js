cb1a06b87c614e5b715f23142e4e1a24a847f503
rezaizadijahromi <rezaizadij2000@gmail.com>
2022-08-19 12:20:43 +0430

    clean up

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -14,6 +14,7 @@ router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 router.route("/:id/deliver").put(protect, admin, updateOrderToDelivered);
 
 export default router;
